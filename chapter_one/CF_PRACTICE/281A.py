d1f3e014bb4200e184d09d12b44fc6a1f48a3ef1
= <vikramaditya@vajire.com>
2021-02-07 00:18:08 +0530

    Word.... rookie mistake

---
+++ chapter_one/CF_PRACTICE/281A.py
@@ -0,0 +1,11 @@
+# https://codeforces.com/problemset/problem/281/A
+v = input()
+c = v
+v = v.upper()
+
+print(v[0], end="")
+print(c[1:])
+
+# (c) 2021, Vikramaditya V. Vajire
+# https://github.com/vikramaditya
+# This source code is licensed under the MIT License
