b4eb654601e39a4b54dc9769a257dbdfeeec55e5
Vikramaditya Vajire <vikramaditya@vajire.com>
2021-03-26 21:45:51 +0530

    CSES Missing Number - basic and fast approach

--- chapter_one/CSES/PROBLEMSET/INTRODUCTORY_PROBLEMS/02_Missing_Number.py
+++ chapter_one/CSES/PROBLEMSET/INTRODUCTORY_PROBLEMS/02_Missing_Number.py
@@ -1,4 +1,19 @@
+# https://cses.fi/problemset/task/1083/
+"""
+Introductory Problems : Missing Number
+"""
+
 n = int(input())
 given = [int(c) for c in input().split()]
-ele = [i for i in range(1, n+1) if i not in given]
-print(ele[0])
+given.sort()
+all = [int(c) for c in range(1,n+1)]
+res = -1
+for i in range(len(given)):
+    if given[i] != all[i]:
+        res = all[i]
+        break
+
+if res != -1:
+    print(res)
+else:
+    print(n)
