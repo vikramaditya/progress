38d40a6d30eb1946f539be392df23ef726a6693f
Vikramaditya Vajire <vikramaditya@vajire.com>
2021-04-03 16:32:03 +0530

    April21 LongChallenge SSCRIPT- CleanCode

--- chapter_one/CODECHEF/CC_CONTESTS/APRIL21C/SSCRIPT.py
+++ chapter_one/CODECHEF/CC_CONTESTS/APRIL21C/SSCRIPT.py
@@ -1,42 +1,12 @@
 import re
 t = int(input())
-
-if t < 1 or t > 10:
-    exit()
-
-sum = 0
 for f in range(t):
     N, K = [int(c) for c in input().split()]
-
-    if N <= 10 ** 4:
-        sum += N
-
-    if sum > 10 ** 4:
-        exit()
-
-    if N > 10 ** 6:
-        exit()
-
-    if N <= 10 ** 6 and sum >= 10 ** 6:
-        exit()
-    if K > N:
-        exit()
-
-    if K < 1:
-        exit()
-
     go = input()
-
-    if len(go) != N:
-        exit()
-
     mine = 0
     res = max(re.findall('((?:' + re.escape('*') + ')*)', go), key = len)
-
     mine = len(res)
-
     final = "NO"
     if mine >= K:
         final = "YES"
-
     print(final)
