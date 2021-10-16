89e78575d49c6dbc4e5a5823d0cc903e2fbf521a
vikramaditya <vikramaditya@vajire.com>
2021-10-17 01:06:10 +0530

    fixed tab length

--- chapter_one/CODECHEF/CC_CONTESTS/SNCKQL21/LUCKYNUM.c
+++ chapter_one/CODECHEF/CC_CONTESTS/SNCKQL21/LUCKYNUM.c
@@ -8,17 +8,17 @@ Date: Sunday 17 October 2021 12:07:27 AM IST
 
 int main(void)
 {
-  int t;
-  scanf("%d", &t);
-  for (int i=0; i<t; ++i) {
-    int go[3];
-    scanf("%d %d %d", &go[0], &go[1], &go[2] );
+    int t;
+    scanf("%d", &t);
+    for (int i=0; i<t; ++i) {
+        int go[3];
+        scanf("%d %d %d", &go[0], &go[1], &go[2] );
 
-    if (go[0] == 7 || go[1] == 7 || go[2] == 7)
-      printf("YES\n");
-    else
-      printf("NO\n");
-  }
+        if (go[0] == 7 || go[1] == 7 || go[2] == 7)
+            printf("YES\n");
+        else
+        printf("NO\n");
+}
 
-  return 0;
+    return 0;
 }
