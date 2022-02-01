deedd7250f5d2fa33a13497aedb6feb99f1e7045
Vikramaditya V. Vajire <vikramaditya@vajire.com>
2022-02-02 01:14:57 +0530

    spaces and tabs
    Signed-off-by: Vikramaditya V. Vajire <vikramaditya@vajire.com>

--- Codeforces/1633A_Div_7.cpp
+++ Codeforces/1633A_Div_7.cpp
@@ -1,6 +1,6 @@
 /**
- * Author	: veep
- * Date  	: Wednesday 02 February 2022 01:00:49 AM IST
+ * Author    : veep
+ * Date      : Wednesday 02 February 2022 01:00:49 AM IST
  *
  * Code     : 158A
  *
@@ -12,28 +12,28 @@ using namespace std;
 
 int main() {
     int n;
-	cin >> n;
+    cin >> n;
 
-	while (n) {
-		int c;
+    while (n) {
+        int c;
         cin >> c;
         int res = 0;
         
         if (c % 7 == 0){
-        	res = c;
+            res = c;
         } else {
-        	res = -1;
-        	for (int i = 0; i < 10; i++) {
-        		if ((c - c % 10 + i) % 7 == 0) {
-        			res = c - c % 10 + i;
-        		}
-        	}
+            res = -1;
+            for (int i = 0; i < 10; i++) {
+                if ((c - c % 10 + i) % 7 == 0) {
+                    res = c - c % 10 + i;
+                }
+            }
         }
 
         cout << res << endl;
 
-		n--;
-	}
+        n--;
+    }
     return 0;
 }
 
